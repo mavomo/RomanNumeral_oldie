ca9743e14c2513620b9c1e6268726ca07bc2abfa
mavomo <michelle.avomo@gmail.com>
2017-01-13 23:51:22 +0100

    Implementation of RomanNumerals kata

--- src/test/java/io.training.kata/RomanNumeral.java
+++ src/test/java/io.training.kata/RomanNumeral.java
@@ -6,40 +6,38 @@ import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Created by Michelle on 13/01/2017.
- *
  * @see : http://literacy.kent.edu/Minigrants/Cinci/romanchart.htm
  */
 public class RomanNumeral {
 
-    public static String convertToArabic(final int number) {
+    public static String convertToRomanNumeral(final int number) {
         Map<Integer, String> arabicNumbers = romanNumeralsChart();
-
-
         AtomicInteger numberToConvert = new AtomicInteger(number);
-        StringBuilder arabicValue = new StringBuilder();
+        StringBuilder romanNumeralValue = new StringBuilder();
         arabicNumbers.keySet().forEach(key -> {
-            while (numberToConvert.intValue() >= key.intValue()){
-                arabicValue.append(arabicNumbers.get(key));
-                numberToConvert.set(numberToConvert.intValue() - key.intValue());
+            int arabicNumber = key.intValue();
+            while (numberToConvert.intValue() >= arabicNumber){
+                romanNumeralValue.append(arabicNumbers.get(key));
+                numberToConvert.set(numberToConvert.intValue() - arabicNumber);
             }
         });
-      return arabicValue.toString();
+      return romanNumeralValue.toString();
     }
 
     private static Map<Integer, String> romanNumeralsChart() {
         Map<Integer, String> arabicNumbers = new TreeMap<>(Comparator.reverseOrder());
-        arabicNumbers.put(1000, "M");
-        arabicNumbers.put(500, "D");
-        arabicNumbers.put(100, "C");
-        arabicNumbers.put(90, "XC");
-        arabicNumbers.put(50, "L");
-        arabicNumbers.put(40, "XL");
-        arabicNumbers.put(10, "X");
-        arabicNumbers.put(9, "IX");
-        arabicNumbers.put(5, "V");
-        arabicNumbers.put(4, "IV");
         arabicNumbers.put(1, "I");
+        arabicNumbers.put(4, "IV");
+        arabicNumbers.put(5, "V");
+        arabicNumbers.put(9, "IX");
+        arabicNumbers.put(10, "X");
+        arabicNumbers.put(40, "XL");
+        arabicNumbers.put(50, "L");
+        arabicNumbers.put(90, "XC");
+        arabicNumbers.put(100, "C");
+        arabicNumbers.put(500, "D");
+        arabicNumbers.put(1000, "M");
+
         return arabicNumbers;
     }
 }
--- src/test/java/io.training.kata/RomanNumerals.java
+++ src/test/java/io.training.kata/RomanNumerals.java
@@ -1,6 +1,5 @@
 package io.training.kata;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,148 +12,148 @@ public class RomanNumerals {
 
     @Test
     public void should_convert_1_to_I(){
-        assertThat(RomanNumeral.convertToArabic(1)).isEqualTo("I");
+        assertThat(RomanNumeral.convertToRomanNumeral(1)).isEqualTo("I");
     }
 
     @Test
     public void should_convert_2_to_II()  {
-        assertThat(RomanNumeral.convertToArabic(2)).isEqualTo("II");
+        assertThat(RomanNumeral.convertToRomanNumeral(2)).isEqualTo("II");
     }
 
     @Test
     public void should_convert_3_to_III() {
-        assertThat(RomanNumeral.convertToArabic(3)).isEqualTo("III");
+        assertThat(RomanNumeral.convertToRomanNumeral(3)).isEqualTo("III");
     }
 
     @Test
     public void should_convert_5_to_V(){
-        assertThat(RomanNumeral.convertToArabic(5)).isEqualTo("V");
+        assertThat(RomanNumeral.convertToRomanNumeral(5)).isEqualTo("V");
     }
 
     @Test
     public void should_convert_6_to_VI() {
-        assertThat(RomanNumeral.convertToArabic(6)).isEqualTo("VI");
+        assertThat(RomanNumeral.convertToRomanNumeral(6)).isEqualTo("VI");
     }
 
     @Test
     public void should_convert_7_to_VII() {
-        assertThat(RomanNumeral.convertToArabic(7)).isEqualTo("VII");
+        assertThat(RomanNumeral.convertToRomanNumeral(7)).isEqualTo("VII");
     }
 
     @Test
     public void should_convert_8_to_VIII() {
-        assertThat(RomanNumeral.convertToArabic(8)).isEqualTo("VIII");
+        assertThat(RomanNumeral.convertToRomanNumeral(8)).isEqualTo("VIII");
     }
 
     @Test
     public void should_convert_10_to_X() {
-        assertThat(RomanNumeral.convertToArabic(10)).isEqualTo("X");
+        assertThat(RomanNumeral.convertToRomanNumeral(10)).isEqualTo("X");
     }
 
     @Test
     public void should_convert_11_to_XI() {
-        assertThat(RomanNumeral.convertToArabic(11)).isEqualTo("XI");
+        assertThat(RomanNumeral.convertToRomanNumeral(11)).isEqualTo("XI");
     }
 
     @Test
     public void shoul_convert_12_to_XII()  {
-        assertThat(RomanNumeral.convertToArabic(12)).isEqualTo("XII");
+        assertThat(RomanNumeral.convertToRomanNumeral(12)).isEqualTo("XII");
     }
 
     @Test
     public void should_convert_15_to_XV (){
-        assertThat(RomanNumeral.convertToArabic(15)).isEqualTo("XV");
+        assertThat(RomanNumeral.convertToRomanNumeral(15)).isEqualTo("XV");
     }
 
     @Test
     public void should_convert_18_to_XVIII() {
-        assertThat(RomanNumeral.convertToArabic(18)).isEqualTo("XVIII");
+        assertThat(RomanNumeral.convertToRomanNumeral(18)).isEqualTo("XVIII");
     }
 
     @Test
     public void should_convert_20_to_XX() {
-        assertThat(RomanNumeral.convertToArabic(20)).isEqualTo("XX");
+        assertThat(RomanNumeral.convertToRomanNumeral(20)).isEqualTo("XX");
     }
 
     @Test
     public void should_convert_21_to_XXI(){
-        assertThat(RomanNumeral.convertToArabic(21)).isEqualTo("XXI");
+        assertThat(RomanNumeral.convertToRomanNumeral(21)).isEqualTo("XXI");
     }
 
     @Test
     public void should_convert_30_to_XXX(){
-        assertThat(RomanNumeral.convertToArabic(30)).isEqualTo("XXX");
+        assertThat(RomanNumeral.convertToRomanNumeral(30)).isEqualTo("XXX");
     }
 
     @Test
     public void should_convert_50_to_L(){
-        assertThat(RomanNumeral.convertToArabic(50)).isEqualTo("L");
+        assertThat(RomanNumeral.convertToRomanNumeral(50)).isEqualTo("L");
     }
 
     @Test
     public void should_convert_55_to_LV(){
-        assertThat(RomanNumeral.convertToArabic(55)).isEqualTo("LV");
+        assertThat(RomanNumeral.convertToRomanNumeral(55)).isEqualTo("LV");
     }
 
     @Test
     public void should_convert_60_to_LX(){
-        assertThat(RomanNumeral.convertToArabic(60)).isEqualTo("LX");
+        assertThat(RomanNumeral.convertToRomanNumeral(60)).isEqualTo("LX");
     }
 
 
     @Test
     public void should_convert_100_to_C(){
-        assertThat(RomanNumeral.convertToArabic(100)).isEqualTo("C");
+        assertThat(RomanNumeral.convertToRomanNumeral(100)).isEqualTo("C");
     }
 
     @Test
     public void should_convert_120_to_C(){
-        assertThat(RomanNumeral.convertToArabic(120)).isEqualTo("CXX");
+        assertThat(RomanNumeral.convertToRomanNumeral(120)).isEqualTo("CXX");
     }
 
     @Test
     public void should_convert_500_to_D(){
-        assertThat(RomanNumeral.convertToArabic(500)).isEqualTo("D");
+        assertThat(RomanNumeral.convertToRomanNumeral(500)).isEqualTo("D");
     }
 
     @Test
     public void should_convert_1000_to_M(){
-        assertThat(RomanNumeral.convertToArabic(1000)).isEqualTo("M");
+        assertThat(RomanNumeral.convertToRomanNumeral(1000)).isEqualTo("M");
     }
 
     @Test
     public void should_convert_4_to_IV()  {
-        assertThat(RomanNumeral.convertToArabic(4)).isEqualTo("IV");
+        assertThat(RomanNumeral.convertToRomanNumeral(4)).isEqualTo("IV");
     }
 
     @Test
     public void should_convert_24_to_XXIV()  {
-        assertThat(RomanNumeral.convertToArabic(24)).isEqualTo("XXIV");
+        assertThat(RomanNumeral.convertToRomanNumeral(24)).isEqualTo("XXIV");
     }
 
     @Test
     public void should_convert_9_to_IX(){
-        assertThat(RomanNumeral.convertToArabic(9)).isEqualTo("IX");
+        assertThat(RomanNumeral.convertToRomanNumeral(9)).isEqualTo("IX");
     }
 
     @Test
     public void should_convert_40_to_XL(){
-        assertThat(RomanNumeral.convertToArabic(40)).isEqualTo("XL");
+        assertThat(RomanNumeral.convertToRomanNumeral(40)).isEqualTo("XL");
     }
 
     @Test
     public void should_convert_49_to_XL(){
-        assertThat(RomanNumeral.convertToArabic(49)).isEqualTo("XLIX");
+        assertThat(RomanNumeral.convertToRomanNumeral(49)).isEqualTo("XLIX");
     }
 
     @Test
     public void should_return_90_to_XC()  {
-        assertThat(RomanNumeral.convertToArabic(90)).isEqualTo("XC");
+        assertThat(RomanNumeral.convertToRomanNumeral(90)).isEqualTo("XC");
     }
 
     @Test
     public void should_return_99_to_XCIX()  {
-        assertThat(RomanNumeral.convertToArabic(99)).isEqualTo("XCIX");
+        assertThat(RomanNumeral.convertToRomanNumeral(99)).isEqualTo("XCIX");
     }
 }
 
